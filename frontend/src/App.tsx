f2b211dfc34915ffaa35f9fa8a878af77bc5d3fb
Aketca <Someone9379992@gmail.com>
2021-12-09 09:50:43 +0200

    save ts problems

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./styles/App.scss";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import { Link } from "react-router-dom";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      todos: [],
-    };
-  }
+const App = () => {
+  const [todos, setTodos] = useState([]);
 
-  componentDidMount() {
+  useEffect(()=>{
     axios
       .get("/api")
       .then((response) => {
-        this.setState({
-          todos: response.data.data,
-        });
+        setTodos(response.data.data);
       })
       .catch((e) => console.log("Error : ", e));
-  }
+  }, []);
 
-  handleAddTodo = (value) => {
+  const handleAddTodo = (value) => {
     axios
       .post("/api/todos", { text: value })
       .then(() => {
-        this.setState({
-          todos: [...this.state.todos, { text: value }],
-        });
+        setTodos([...todos, { text: value }]);
       })
       .catch((e) => console.log("Error : ", e));
   };
-
-  render() {
-    return (
-      <div className="App container">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
-              <nav>
-                <Link to="/page1">Page1</Link>
-                <Link to="/page2">Page2</Link>
-              </nav>
-              <h1>Todos</h1>
-              <div className="todo-app">
-                <AddTodo handleAddTodo={this.handleAddTodo} />
-                <TodoList todos={this.state.todos} />
-              </div>
+  return (
+    <div className="App container">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
+            <nav>
+              <Link to="/page1">Page1</Link>
+              <Link to="/page2">Page2</Link>
+            </nav>
+            <h1>Todos</h1>
+            <div className="todo-app">
+              <AddTodo handleAddTodo={handleAddTodo} />
+              <TodoList todos={todos} />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+export default  App;
--- frontend/src/components/AddTodo.tsx
+++ frontend/src/components/AddTodo.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 
-export default class AddTodo extends React.Component {
-  handleSubmit = (e) => {
+const addTodo = ({handleAddTodo}) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target.elements.value;
     if (value.length > 0) {
-      this.props.handleAddTodo(value);
+      handleAddTodo(value);
       e.target.reset();
     }
   };
 
-  render() {
-    return (
-      <form
-        noValidate
-        onSubmit={this.handleSubmit}
-        className="new-todo form-group"
-      >
-        <input
-          type="text"
-          name="value"
-          required
-          minLength={1}
-          className="form-control"
-        />
-        <button className="btn btn-primary" type="submit">
-          Add Todo
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form
+      noValidate
+      onSubmit={handleSubmit}
+      className="new-todo form-group"
+    >
+      <input
+        type="text"
+        name="value"
+        required
+        minLength={1}
+        className="form-control"
+      />
+      <button className="btn btn-primary" type="submit">
+        Add Todo
+      </button>
+    </form>
+  )
 }
+
+export default addTodo();
